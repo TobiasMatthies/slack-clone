f0e9f438be97542a000951f9f9374a937d640022
TobiasMatthies <tbsmatthies@gmail.com>
2023-05-31 14:57:27 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { ChatComponent } from './home/chat/chat.component';
+import { ThreadComponent } from './home/thread/thread.component';
+import { HeaderComponent } from './home/header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ChatComponent,
+    ThreadComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue1: "#5764F2",
+        green_online: "#21A559",
+        green1: "248046",
+        pink_error: "#ED1E79",
+        dark1: "#303338",
+        dark2: "#2A2D31",
+        dark3: "#1E1F22",
+        bg_gray: "#3F4248",
+        gray_icon: "80848E",
+      },
+    },
+  },
+  plugins: [],
+};
+
